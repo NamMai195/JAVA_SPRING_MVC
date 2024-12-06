15a5e8698014df7e530cce7e8603435692ba42d1
NamMai195 <mnam3239@gmail.com>
2024-12-06 12:38:31 +0700

    test

---
+++ lab01-java-spring-mvc/src/main/java/edu/poly/lab01/Lab01JavaSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package edu.poly.lab01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab01JavaSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab01JavaSpringMvcApplication.class, args);
+    }
+
+}
