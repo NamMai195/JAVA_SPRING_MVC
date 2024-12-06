831c67da5d1debb1d7c33c5e8871ac82829716e6
NamMai195 <mnam3239@gmail.com>
2024-12-06 12:56:04 +0700

    hello word

---
+++ lab01-java-spring-mvc/src/main/java/edu/poly/lab01/HelloController.java
@@ -0,0 +1,12 @@
+package edu.poly.lab01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "Con Cu 10 m";
+    }
+}
