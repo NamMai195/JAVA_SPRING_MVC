100d4dfe733b3f189ae2c35a2456e1bd84629eec
NamMai195 <mnam3239@gmaill.com>
2024-12-18 11:47:42 -0500

    update pom mysql and security

--- lab01-java-spring-mvc/src/main/java/edu/poly/lab01/HelloController.java
+++ lab01-java-spring-mvc/src/main/java/edu/poly/lab01/HelloController.java
@@ -5,8 +5,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+
     @GetMapping("/")
     public String index() {
-        return "Con Cu 10 m";
+        return "Con Cu 20 m";
+    }
+    @GetMapping("/user")
+    public String userPage() {
+        return "User Page";
+    }
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Admin Page";
     }
 }
---
+++ lab01-java-spring-mvc/src/main/java/edu/poly/lab01/service/MyService.java
@@ -0,0 +1,7 @@
+package edu.poly.lab01.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+}
