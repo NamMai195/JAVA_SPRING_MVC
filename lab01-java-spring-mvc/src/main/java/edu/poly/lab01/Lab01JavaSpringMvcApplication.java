4ba9f5b2b0bb138126f00dfa6426e82dd39ccd8d
NamMai195 <mnam3239@gmaill.com>
2024-12-19 00:10:54 -0500

    disable security.

--- lab01-java-spring-mvc/src/main/java/edu/poly/lab01/Lab01JavaSpringMvcApplication.java
+++ lab01-java-spring-mvc/src/main/java/edu/poly/lab01/Lab01JavaSpringMvcApplication.java
@@ -3,7 +3,9 @@ package edu.poly.lab01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude =
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class Lab01JavaSpringMvcApplication {
 
     public static void main(String[] args) {
